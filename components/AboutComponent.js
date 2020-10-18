ac40c21fa3eb1ff1a2fa3453576e758b94941cf3
aimable uwitonze <aimable_uwitonze@yahoo.com>
2020-10-18 12:43:32 -0500

    added comment

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -26,6 +26,7 @@ class About extends Component {
                 />
             );
         };
+        // return the flatlist
         return (
             <ScrollView>
                 <Mission />
